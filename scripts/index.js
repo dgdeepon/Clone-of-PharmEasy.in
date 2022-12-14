053a50539fe0b7e47640eaf59ddc92b48285c257
Deepon Ghosh <deepanghoshslg@gmail.com>
2022-12-14 21:32:56 +0530

    Day-2

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
-// Lab Test Slideshow
-var swiper = new Swiper(".mySwiper", {
+
+
+  // Lab Test Slideshow
+  var swiper = new Swiper(".mySwiper", {
     slidesPerView: 6.5,
     spaceBetween: 30,
     slidesPerGroup: 2,
@@ -10,3 +12,38 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+  // New Launches
+  let launchDisplay=document.getElementById("newLaunchProduct");
+  fetch("data/newLaunchData.json")
+  .then((rec)=>{
+    return rec.json();
+  })
+  .then((res)=>{
+    console.log(res);
+    createProduct(res);
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+
+  // Product Creating
+  function createProduct(data){
+    data.forEach(function (element,index){
+      let box=document.createElement("div");
+      box.setAttribute("class","swiper-slide shopbycate");
+      box.style.width="10%";
+      let image=document.createElement("img");
+      image.setAttribute("src",element.image);
+      let title=document.createElement("h3");
+      title.innerText=element.product;
+      title.setAttribute("class","titleSize");
+      let price=document.createElement("p");
+      price.innerText="MRP ₹"+element.price;
+      box.append(image,title,price);
+      launchDisplay.append(box);
+    })
+  }
+
+
+
