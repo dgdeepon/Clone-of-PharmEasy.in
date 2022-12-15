1addbf6e4fa59608e2cf93532abab10d4df9cc73
Deepon Ghosh <deepanghoshslg@gmail.com>
2022-12-15 14:26:18 +0530

    Day-3 10%

--- scripts/index.js
+++ scripts/index.js
@@ -36,9 +36,13 @@
       image.setAttribute("src",element.image);
       let title=document.createElement("h3");
       title.innerText=element.product;
+      title.style.color="rgb(71, 70, 70)";
+      title.style.fontSize="85%";
       title.setAttribute("class","titleSize");
       let price=document.createElement("p");
       price.innerText="MRP ₹"+element.price;
+      price.style.color="grey";
+      price.style.fontSize="85%";
       box.append(image,title,price);
       launchDisplay.append(box);
     })
